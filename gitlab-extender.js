57a62fe15f4655722bb97ed0eda6c7314389131b
kocsis-antal <46327610+kocsis-antal@users.noreply.github.com>
2025-04-23 11:08:13 +0200

    Update gitlab-extender.js

--- gitlab-extender.js
+++ gitlab-extender.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gitlab extender
 // @namespace    https://github.com/kocsis-antal/tampermonkey/
-// @version      1.0.20250423-1101
+// @version      1.0.20250423-1108
 // @updateURL    https://raw.githubusercontent.com/kocsis-antal/tampermonkey/refs/heads/master/gitlab-extender.js
 // @downloadURL  https://raw.githubusercontent.com/kocsis-antal/tampermonkey/refs/heads/master/gitlab-extender.js
 // @description  gitlab MR coloring and extra MR button
